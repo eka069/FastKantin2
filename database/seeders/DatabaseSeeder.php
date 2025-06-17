2fb3c3cbe64b4bbff5163009da3afab66885847c
cintarmynti <ramayanticinta@gmail.com>
2025-06-17 18:36:54 +0700

    menghapuus seeder yg tidak digunakan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,9 +18,9 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            MerchantSeeder::class,
-            CategoriesSeeder::class,
-            FoodItemSeeder::class,
+            // MerchantSeeder::class,
+            // CategoriesSeeder::class,
+            // FoodItemSeeder::class,
 
         ]);
     }
